000f2a0a1e7682b37ffb9fee994b65cfa8cd555a
mbartram <mccormickb16@gmail.com>
2017-04-08 16:35:26 -0400

    color changes

--- Calculator/Calculator/CalculatorEngine.swift
+++ Calculator/Calculator/CalculatorEngine.swift
@@ -119,7 +119,6 @@ struct CalculatorBrain {
     private struct PendingBinaryOperation {
         let function: (Double,Double) -> Double
         let firstOperand: Double
-        
         func perform(with secondOperand: Double) -> Double {
             return function(firstOperand, secondOperand)
         }
